a59ad78d419e4cd5526efb3497f585b10a18ef25
Patrick Kim <pkim050@gmail.com>
2019-05-27 04:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const newDriver = map(drivers, function(driver) {
+    return Object.assign({}, driver, { driver.toLowerCase() });
+  });
+}
